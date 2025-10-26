d8ccf0de0ac507cbc6185c74add497de05629c11
bryan-000 <fredayddd321ewq@gmail.com>
2025-10-26 18:01:02 +0000

    fix non-solid enemies

--- stupid ahh fix.cs
+++ stupid ahh fix.cs
@@ -36,9 +36,83 @@ public static class FixOutdoorEnemies
     [HarmonyPatch(typeof(Guttertank), "Start")]
     public static void GuttertankFix(Guttertank __instance)
     {
-        var gameobject = new ChildGetter<Guttertank>(__instance).Get("Guttertank/Guttertank");
+        GameObject gameobject = new ChildGetter<Guttertank>(__instance).Get("Guttertank/Guttertank");
         log.Info($"GuttertankFix:: gameobject.name: {gameobject.name} gameobject.layer: {gameobject.layer}");
         AddForcer(gameobject);
+    }
+
+    /// <summary> Fixes the visibility of the Gutterman in an Impact Frame. </summary>
+    /// <param name="__instance">Instance of the Gutterman.</param>
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Gutterman), "Start")]
+    public static void GuttermanFix(Gutterman __instance)
+    {
+        ChildGetter<Gutterman> getter = new(__instance);
+        List<GameObject> gameObjects = 
+            [getter.Get("Gutterman"), getter.Get("Gutterman/Gutterman"), getter.Get("Gutterman/Shield")];
+        
+        AddForcer(gameObjects);
+    }
+
+    /// <summary> Fixes the visibility of the MortarLauncher in an Impact Frame. </summary>
+    /// <param name="__instance">Instance of the MortarLauncher.</param>
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MortarLauncher), "Start")]
+    public static void MortarLauncherFix(MortarLauncher __instance)
+    {
+        bool isTower = __instance.transform.Find("Tower") != null;
+        GameObject gameobject = new ChildGetter<MortarLauncher>(__instance).Get(isTower ? "Tower" : "MortarLauncher");
+        AddForcer(gameobject);
+    }
+
+    /// <summary> Fixes the visibility of the Maurice in an Impact Frame. </summary>
+    /// <param name="__instance">Instance of the Maurice.</param>
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SpiderBody), "Start")]
+    public static void MauriceFix(SpiderBody __instance)
+    {
+        GameObject gameobject = new ChildGetter<SpiderBody>(__instance).Get("MaliciousFace/MaliciousFace");
+        AddForcer(gameobject);
+    }
+
+    /// <summary> Fixes the visibility of the Cerberi in an Impact Frame. </summary>
+    /// <param name="__instance">Instance of the Cerberi.</param>
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Statue), "Start")]
+    public static void CerberiFix(Statue __instance)
+    {
+        ChildGetter<Statue> getter = new(__instance);
+        List<GameObject> gameObjects =
+            [getter.Get("Cerberus/Cerberus"), getter.Get("Cerberus/Cerb_Apple")];
+
+        AddForcer(gameObjects);
+    }
+
+    /// <summary> Fixes the visibility of the Minotaur in an Impact Frame. </summary>
+    /// <param name="__instance">Instance of the Minotaur.</param>
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Minotaur), "Start")]
+    public static void MinotaurFix(Minotaur __instance)
+    {
+        ChildGetter<Minotaur> getter = new(__instance);
+        List<GameObject> gameObjects =
+            [getter.Get("Minotaur_Rigging02/Minotaur"), getter.Get("Minotaur_Rigging02/Minotaur_Staff")];
+
+        AddForcer(gameObjects);
+    }
+
+    /// <summary> Fixes the visibility of the Mandalore in an Impact Frame. </summary>
+    /// <param name="__instance">Instance of the Mandalore.</param>
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Mandalore), "Start")]
+    public static void MandaloreFix(Mandalore __instance)
+    {
+        ChildGetter<Mandalore> getter = new(__instance);
+        List<GameObject> gameObjects =
+            [getter.Get("Mandalore (Skeleton)/Skeleton"), getter.Get("Mandalore (Skeleton)/Armature.001/Base/Hips/Chest/Shammy/ShammyMesh"), 
+            getter.Get("Mandalore (Skeleton)/Armature.001/Base/Hips/Chest/Neck/Head/Cylinder/Mandalore_Head")];
+
+        AddForcer(gameObjects);
     }
 
     /// <summary> Fixes the visibility of the Panopticon in an Impact Frame. </summary>
@@ -50,7 +124,7 @@ public static class FixOutdoorEnemies
         ChildGetter<FleshPrison> getter = new(__instance);
         List<GameObject> gameObjects = __instance.eid.FullName == "FLESH PANOPTICON"
             ? [getter.Get("FleshPrison2"), getter.Get("FleshPrison2/FleshPrison2_Base"), getter.Get("FleshPrison2/FleshPrison2_Head")]
-            : [];
+            : [getter.Get("fleshprisonrigged/FleshPrison")];
 
         AddForcer(gameObjects);
     }
